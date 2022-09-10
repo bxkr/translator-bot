43f297e22c1ce0415ab434f9ec4969986f1f7e6e
Andrey <rielyalkov1@gmail.com>
2022-09-10 21:59:47 +0300

    Type result

--- webapp.py
+++ webapp.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from flask import Flask, render_template, request
 
@@ -13,9 +15,18 @@ def root():
 def result():
     args = request.args['query']
     word = request.args['word']
+    res = {
+        'type': 'article',
+        'id': 0,
+        'title': 'Result',
+        'input_message_content': {
+            'message_text': word
+        }
+    }
     base_url = f'https://api.telegram.org/bot5709327658:AAHiO3kEZorMxAXKOe5qEJOtajO0CATYo4Y' \
-               f'/answerWebAppQuery?web_app_query_id={args}&result={{"word": "{word}"}}'
-    requests.get(base_url)
+               f'/answerWebAppQuery?web_app_query_id={args}&result={json.dumps(res)}'
+    req = requests.get(base_url)
+    print(req.json())
 
 
 @app.get('/<path:path>')
